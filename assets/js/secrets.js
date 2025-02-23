ec08b4e16efcf15b17af3ba88e47d0a4e01e5376
Michael Lizzio <mjl2396@rit.edu>
2025-02-22 19:32:18 -0500

    pain

--- assets/js/secrets.js
+++
@@ -1,6 +0,0 @@
-// secrets.js
-
-// IMPORTANT: In a production environment, do not expose your API key on the client side.
-// This file is for demonstration purposes only.
-require('dotenv').config();
-const apiKey = process.env.OPENAI_API_KEY;
